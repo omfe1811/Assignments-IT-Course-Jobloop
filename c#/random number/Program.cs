fa469c515d890d66446e3081caa75d58e01e1dd8
Olga <omfe1811@gmail.com>
2024-12-06 15:42:57 +0100

    random number generator

---
+++ c#/random number/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace randomNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            double randomNumber = random.NextInt64();
+
+            Console.WriteLine($"Random number: {randomNumber}");
+        }
+
+
+    }
+}
+
